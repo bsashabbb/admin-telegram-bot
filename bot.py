f52166197cd04b2b29438ab64b385cfdd7783a71
bsashabbb <bsashabbb@yandex.ru>
2024-07-23 23:28:10 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
+import telebot
 
+bot = telebot.TeleBot('token')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+bot.infinity_polling()
